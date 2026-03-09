657e0d6ab41c343674735524978d582733399e39
Arya Mulay <aryasmulay@gmail.com>
2026-03-09 09:24:40 +0530

    Deleting directory

--- web_sim/src/app/(dashboard)/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-import { DashboardShell } from "@/components/layout/DashboardShell";
-
-export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <DashboardShell>{children}</DashboardShell>;
-}
-
